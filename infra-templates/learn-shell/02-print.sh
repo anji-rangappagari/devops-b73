eabc27812e6169f9a6bf62c8e57c25ee39bda06e
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-22 22:19:36 +0800

    color coding

--- infra-templates/learn-shell/02-print.sh
+++ infra-templates/learn-shell/02-print.sh
@@ -9,4 +9,19 @@ echo "Hello World"
 
 #\e[COLm - To enable color, where COL is the color code]
 #\e[0m - To reset the color back to default
+echo -e "\e[30mHello \nWorld\e[0m"
+echo -e "\e[31mHello \nWorld\e[0m"
 echo -e "\e[32mHello \nWorld\e[0m"
+echo -e "\e[33mHello \nWorld\e[0m"
+echo -e "\e[34mHello \nWorld\e[0m"
+echo -e "\e[35mHello \nWorld\e[0m"
+echo -e "\e[36mHello \nWorld\e[0m"
+
+# COL stands for color and possible color codes are as below
+# 30 - Black
+# 31 - Red
+# 32 - Green
+# 33 - Yellow
+# 34 - Blue
+# 35 - Magenta
+# 36 - Cyan     
