373024b73ff9b8fc54ec21cff4c126f51e211bc8
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-23 01:36:32 +0800

    redirctors output+error

--- infra-templates/roboshop-shell/01-frontend/frontend.sh
+++ infra-templates/roboshop-shell/01-frontend/frontend.sh
@@ -1,17 +1,17 @@
 echo -e "\e[33mInstalling Nginx Server\e[0m"
-dnf install nginx -y >/tmp/roboshop.log
+dnf install nginx -y &>/tmp/roboshop.log
 echo -e "\e[33mRemoving old app content\e[0m"
-rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*  &>/tmp/roboshop.log
 
 echo -e "\e[33m Downloading the front end content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>/tmp/roboshop.log
 
 echo -e "\e[33m exatracting the front end content\e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip >/tmp/roboshop.log
+unzip /tmp/frontend.zip &>/tmp/roboshop.log
 
 # We need to copy the config file to the nginx config directory
 
 echo -e "\e[33m Starting Nginx service\e[0m"
-systemctl enable nginx  >/tmp/roboshop.log
-systemctl restart nginx  >/tmp/roboshop.log
+systemctl enable nginx  &>/tmp/roboshop.log
+systemctl restart nginx  &>/tmp/roboshop.log
