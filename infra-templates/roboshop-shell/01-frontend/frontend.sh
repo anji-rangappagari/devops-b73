d5125a6c3a959225c26b89e9d2d70956f34ea074
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-23 01:02:33 +0800

    frontend.sh update

--- infra-templates/roboshop-shell/01-frontend/frontend.sh
+++ infra-templates/roboshop-shell/01-frontend/frontend.sh
@@ -1,15 +1,8 @@
-
 dnf install nginx -y 
-
 rm -rf /usr/share/nginx/html/* 
-
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
-
-# Add the following content
-#cp  /etc/nginx/default.d/roboshop.conf
-
+# We need to copy the config file to the nginx config directory
 systemctl enable nginx 
 systemctl restart nginx 
