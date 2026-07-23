147c4ef8f3795399388e17f4b08c38664ff25a5b
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-23 14:54:40 +0800

    catalogue fix

--- infra-templates/roboshop-shell/catalogue.sh
+++ infra-templates/roboshop-shell/catalogue.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo -e "\e[33mInstalling Node.js\e[0m"
 dnf module list nodejs &>>/tmp/roboshop.log
 
@@ -15,29 +17,28 @@ echo -e "\e[33mCreating the application directory\e[0m"
 mkdir -p /app &>>/tmp/roboshop.log
 
 echo -e "\e[33mDownloading the application content\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  $>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
 cd /app 
 
 echo -e "\e[33mExtracting the application content\e[0m"
-unzip /tmp/catalogue.zip $>>/tmp/roboshop.log
-cd /app
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstalling the application dependencies\e[0m"
+cd /app
 npm install &>>/tmp/roboshop.log
 
-cp catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting the catalogue service\e[0m"
-systemctl daemon-reload  &>>/tmp/roboshop.log
+systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl start catalogue  &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying the MongoDB repo file\e[0m"
-cp mogodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log 
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log 
 
-echo -e "\e[33mInstalling MongoDB\e[0m"
-dnf install mongodb-org -y &>>/tmp/roboshop.log"
+echo -e "\e[33mInstalling MongoDB Client\e[0m"
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
+echo -e "\e[33mLoading Schema\e[0m"
 mongosh --host mongodb-dev.oneseven.space </app/db/master-data.js &>>/tmp/roboshop.log
-
-
