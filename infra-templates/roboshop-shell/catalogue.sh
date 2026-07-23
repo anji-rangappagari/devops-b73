675c4596076faa79f0936881d471a9e05dd75fd6
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-23 14:19:59 +0800

    catalogue.sh

---
+++ infra-templates/roboshop-shell/catalogue.sh
@@ -0,0 +1,46 @@
+echo -e "\e[33mInstalling Node.js\e[0m"
+dnf module list nodejs &>>/tmp/roboshop.log
+
+echo -e "\e[33mEnabling Node.js module\e[0m"
+dnf module disable nodejs:18 -y &>>/tmp/roboshop.log
+dnf module enable nodejs:16 -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mInstalling Node.js\e[0m"
+dnf install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33mAdding the application user\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[33mCreating the application directory\e[0m"
+mkdir -p /app &>>/tmp/roboshop.log
+
+echo -e "\e[33mDownloading the application content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  $>>/tmp/roboshop.log
+cd /app 
+
+echo -e "\e[33mExtracting the application content\e[0m"
+unzip /tmp/catalogue.zip $>>/tmp/roboshop.log
+
+cd /app
+
+echo -e "\e[33mInstalling the application dependencies\e[0m"
+npm install &>>/tmp/roboshop.log
+
+cp /home/ec2-user/devops-b73/infra-templates/roboshop-shell
+catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[33mStarting the catalogue service\e[0m"
+systemctl daemon-reload  &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue  &>>/tmp/roboshop.log
+
+echo -e "\e[33mCopying the MongoDB repo file\e[0m"
+cp mogodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log 
+
+echo -e "\e[33mInstalling MongoDB\e[0m"
+dnf install mongodb-org -y &>>/tmp/roboshop.log"
+
+echo -e "\e[33mLoading the schema\e[0m"
+mongosh --host mongodb-dev.oneseven.space </app/db/master-data.js
+
+
--- infra-templates/roboshop-shell/frontend.sh
+++ infra-templates/roboshop-shell/frontend.sh
@@ -11,8 +11,9 @@ echo -e "\e[33m exatracting the front end content\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-# We need to copy the config file to the nginx config directory
-
+echo -e "\e[33m Copying the roboshop.conf file\e[0m"
+cp /home/ec2-user/devops-b73/infra-templates/roboshop-shell
+roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>/tmp/roboshop.log
 echo -e "\e[33m Starting Nginx service\e[0m"
 systemctl enable nginx  &>>/tmp/roboshop.log
 systemctl restart nginx  &>>/tmp/roboshop.log
