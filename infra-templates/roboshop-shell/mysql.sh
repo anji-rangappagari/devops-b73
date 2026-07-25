99d3d29dbb81294dcdc99035a9ea84288f043af1
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-25 12:18:51 +0800

    mysql

--- infra-templates/roboshop-shell/mysql.sh
+++ infra-templates/roboshop-shell/mysql.sh
@@ -1,19 +1,24 @@
-echo -e "\e[33mDisabling MySQL Module\e[0m"
+#!/bin/bash
+
+echo -e "\e[33mDisabling Default MySQL Module\e[0m"
+dnf module reset mysql -y &>>/tmp/roboshop.log
 dnf module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying MySQL Repository Configuration\e[0m"
 cp /home/ec2-user/devops-b73/infra-templates/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33mInstalling MySQL\e[0m"
-dnf install mysql-community-server -y  &>>/tmp/roboshop.log
+echo -e "\e[33mCleaning DNF Cache\e[0m"
+dnf clean all &>>/tmp/roboshop.log
 
+echo -e "\e[33mInstalling MySQL 5.7 Explicitly\e[0m"
+dnf install --enablerepo=mysql57-community --disablerepo=mysql80-community mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting MySQL Service\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 systemctl start mysqld &>>/tmp/roboshop.log
 
 echo -e "\e[33mSetting up roboshop password\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
 
 echo -e "\e[33mChecking if the password is working?\e[0m"
-mysql -uroot -pRoboShop@1
+mysql -uroot -pRoboShop@1 -e "SELECT VERSION();"
