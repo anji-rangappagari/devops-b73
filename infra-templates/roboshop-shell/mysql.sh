47c8565a6411e5b26dbdb1524f55bba15a1ee783
anji-rangappagari <anji.rangappagari1@gmail.com>
2026-07-25 12:52:53 +0800

    mysql

--- infra-templates/roboshop-shell/mysql.sh
+++ infra-templates/roboshop-shell/mysql.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 echo -e "\e[33mDisabling Default MySQL Module\e[0m"
-dnf module reset mysql -y &>>/tmp/roboshop.log
 dnf module disable mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying MySQL Repository Configuration\e[0m"
 cp /home/ec2-user/devops-b73/infra-templates/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33mCleaning DNF Cache\e[0m"
-dnf clean all &>>/tmp/roboshop.log
-
 echo -e "\e[33mInstalling MySQL 5.7 Explicitly\e[0m"
-dnf install --enablerepo=mysql57-community --disablerepo=mysql80-community mysql-community-server -y &>>/tmp/roboshop.log
+dnf install mysql-community-server -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mStarting MySQL Service\e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
